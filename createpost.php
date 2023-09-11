2883d6cfbd8f7f7b5775df32d78852081385c5a9
yashnaiknavre <yashvnaiknavre@gmail.com>
2023-09-12 00:39:06 +0530

    y

--- createpost.php
+++ createpost.php
@@ -47,7 +47,7 @@
     </main>
     <div>
 <?php 
-// include "inc/footer.php";
+include "inc/footer.php";
 ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
